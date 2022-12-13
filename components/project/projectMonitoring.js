9065227d465c89438495e0a24b2094f6edb3c230
14473HJM <14473@tecnicatura.frc.utn.edu.ar>
2022-12-13 00:04:44 -0300

    Add validations and repo list to monitoring page

--- components/project/projectMonitoring.js
+++ components/project/projectMonitoring.js
@@ -3,40 +3,61 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import IconButton from "@mui/material/IconButton";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from '@mui/icons-material/Edit';
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 import Divider from "@mui/material/Divider";
-import {getInternetPlatforms} from "../../pages/api/config/internetPlatformsApi";
-import {useSession} from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 export default function ProjectMonitoring(props) {
 
     const { data: session } = useSession();
 
-    const [project, setProject]  = React.useState(props.project);
+    const [project, setProject] = React.useState(props.project);
     const [isDisabled, setIsDisabled] = React.useState(props.isDisabled);
     const [repositories, setRepositories] = React.useState([]);
-    const [currentRepository, setCurrentRepository] = React.useState({});
-    const [issueTracker, setIssueTracker] = React.useState();
+    const [currentRepository, setCurrentRepository] = React.useState({
+        internetPlatform: '',
+        ownerName: '',
+        repositoryLink: '',
+        productionBranchName: '',
+    });
+    const [issueTracker, setIssueTracker] = React.useState({
+        internetPlatform: '',
+        projectLink: '',
+        projectName: '',
+        ownerName: '',
+    });
     const [internetPlatforms, setInternetPlatforms] = React.useState(props.availableInternetPlatforms);
-    console.log(props);
+    const [enableSaveButton, setEnableSaveButton] = React.useState(false);
 
     const handleSubmit = (event) => {
 
     }
     const handleAddRepository = () => {
         setRepositories([
-            ...repositories, currentRepository ,
+            ...repositories, currentRepository,
         ]);
-        setCurrentRepository({});
+        setCurrentRepository({
+            internetPlatform: '',
+            ownerName: '',
+            repositoryLink: '',
+            productionBranchName: '',
+        });
+    }
+    const handleDeleteRepository = index => {
+        const newRepositories = [...repositories];
+        newRepositories.splice(index, 1);
+        setRepositories(newRepositories);
     }
     const handleIssueTrackerChanges = (event) => {
         event.preventDefault();
@@ -46,7 +67,6 @@ export default function ProjectMonitoring(props) {
             ...issueTracker,
             [field]: value,
         });
-        console.log(issueTracker);
     }
     const handleRepositoryChanges = (event) => {
         event.preventDefault();
@@ -56,7 +76,6 @@ export default function ProjectMonitoring(props) {
             ...currentRepository,
             [field]: value,
         });
-        console.log(currentRepository);
     }
     const handleIssueTrackerInternetPlatformChanges = (event) => {
         event.preventDefault();
@@ -65,27 +84,25 @@ export default function ProjectMonitoring(props) {
             ...issueTracker,
             ['internetPlatform']: internetPlatform,
         });
-        console.log(event);
-        console.log(issueTracker);
     }
-    const handleRepositoryInternetPlatformChanges = (event) => {
-        event.preventDefault();
-        const internetPlatform = internetPlatforms[event.target.value];
-        setCurrentRepository({
-            ...currentRepository,
-            ['internetPlatform']: internetPlatform,
+
+    React.useEffect(() => {
+        const issueTrackerIsValid = Object.keys(issueTracker).every(key => !!issueTracker[key] === true);
+        const repositoriesAreValid = repositories.length > 0 && repositories.every(repo => {
+            return Object.keys(repo).every(key => !!repo[key] === true);
         });
-        console.log(event);
-        console.log(currentRepository);
-    }
+        console.log('ISSUE TRACKER: ', issueTrackerIsValid)
+        console.log('REPOSITORIES: ', repositoriesAreValid)
+        setEnableSaveButton(issueTrackerIsValid && repositoriesAreValid);
+    }, [issueTracker, repositories])
 
     return (
         <React.Fragment>
             <Box component="form"
-                 noValidate sx={{ mt: 1 }}
-                 alignItems="center"
-                 disabled={isDisabled}
-                 onSubmit={handleSubmit}
+                noValidate sx={{ mt: 1 }}
+                alignItems="center"
+                disabled={isDisabled}
+                onSubmit={handleSubmit}
             >
                 <Grid container spacing={1} >
                     <Grid item xs={12} sm={12}>
@@ -116,7 +133,7 @@ export default function ProjectMonitoring(props) {
                         >
                             {internetPlatforms ? internetPlatforms.map((i, index) =>
                                 i.type == "ISSUE_TRACKER" ?
-                                <MenuItem id={index} key={i.id} value={index}>{i.name}</MenuItem> : null
+                                    <MenuItem id={index} key={i.id} value={index}>{i.name}</MenuItem> : null
                             ) : <MenuItem id={-1} value="None">No disponible</MenuItem>
                             }
                         </Select>
@@ -142,7 +159,7 @@ export default function ProjectMonitoring(props) {
                     </Grid>
                     <Grid item xs={12} sm={6}>
                         <Typography variant="h8" gutterBottom>
-                            Usuario wner del proyecto
+                            Usuario owner del proyecto
                         </Typography>
                     </Grid>
                     <Grid item xs={12} sm={6}>
@@ -170,10 +187,11 @@ export default function ProjectMonitoring(props) {
                             autoComplete="ownerName"
                             defaultValue={project.issueTracker ? project.issueTracker.ownerName : null}
                             disabled={isDisabled}
+                            onChange={handleIssueTrackerChanges}
                         />
                     </Grid>
                 </Grid>
-                <Divider sx={{m:3}}/>
+                <Divider sx={{ m: 3 }} />
                 <Grid container alignItems="center" spacing={1} >
                     <Grid item xs={12} sm={12}>
                         <Typography variant="h6" gutterBottom>
@@ -203,68 +221,84 @@ export default function ProjectMonitoring(props) {
                     {repositories.length > 0 &&
                         <Grid item xs={12} sm={12}>
                             <List>
-                                {repositories.map(item => (
-                                    <ListItem key={`${item.scope}-list-item`}>
-                                        <ListItemText
-                                            primary={item.scope}
-                                        />
+                                {repositories.map((repo, i) => (
+                                    <ListItem
+                                        key={`${repo.internetPlatform}-${i}-list-item`}
+                                        sx={{ pr: 0 }}
+
+                                    >
+                                        <Grid container item xs={12} sm={12} columnSpacing={13.5}>
+                                            <Grid item sm={2}>{internetPlatforms[repo.internetPlatform].name}</Grid>
+                                            <Grid item sm={2}>{repo.ownerName}</Grid>
+                                            <Grid item sm={5}><Link href={repo.repositoryLink} targproductionBranchNameet="_blank">{repo.repositoryLink}</Link></Grid>
+                                            <Grid item sm={2}>{repo.productionBranchName}</Grid>
+                                        </Grid>
+                                        <Grid item xs={12} sm={1} alignItems="center" textAlign={"center"} paddingRight={'22px'}>
+                                            <Fab color="error" aria-label="add" size="small" onClick={() => handleDeleteRepository(i)} >
+                                                <DeleteIcon />
+                                            </Fab>
+                                        </Grid>
                                     </ListItem>
                                 ))}
                             </List>
                         </Grid>
                     }
                     <Grid item xs={12} sm={2} >
-                                <Select
-                                    labelId="internetPlatform"
-                                    id="internetPlatform"
-                                    name="internetPlatform"
-                                    fullWidth
-                                    required
-                                    disabled={isDisabled}
-                                    onChange={handleRepositoryInternetPlatformChanges}
-                                >
-                                    {internetPlatforms ? internetPlatforms.map((i, index) =>
-                                        i.type == "REPOSITORY" ?
-                                            <MenuItem id={index} key={i.id} value={index}>{i.name}</MenuItem> : null
-                                    ) : <MenuItem id={-1} value="None">No disponible</MenuItem>
-                                    }
-                                </Select>
-                            </Grid>
+                        <Select
+                            labelId="internetPlatform"
+                            id="internetPlatform"
+                            name="internetPlatform"
+                            fullWidth
+                            required
+                            disabled={isDisabled}
+                            onChange={handleRepositoryChanges}
+                            value={currentRepository.internetPlatform}
+                        >
+                            {internetPlatforms ? internetPlatforms.map((i, index) =>
+                                i.type == "REPOSITORY" ?
+                                    <MenuItem id={index} key={i.id} value={index}>{i.name}</MenuItem> : null
+                            ) : <MenuItem id={-1} value="None">No disponible</MenuItem>
+                            }
+                        </Select>
+                    </Grid>
                     <Grid item xs={12} sm={2}>
-                                <TextField
-                                    required
-                                    requiredMessage="This field is required."
-                                    errorTarget="under"
-                                    id="ownerName"
-                                    name="ownerName"
-                                    fullWidth
-                                    autoComplete="ownerName"
-                                    disabled={isDisabled}
-                                    onChange={handleRepositoryChanges}
-                                />
-                            </Grid>
+                        <TextField
+                            required
+                            requiredMessage="This field is required."
+                            errorTarget="under"
+                            id="ownerName"
+                            name="ownerName"
+                            fullWidth
+                            autoComplete="ownerName"
+                            disabled={isDisabled}
+                            onChange={handleRepositoryChanges}
+                            value={currentRepository.ownerName}
+                        />
+                    </Grid>
                     <Grid item xs={12} sm={5}>
-                                    <TextField
-                                        required
-                                        id="repositoryLink"
-                                        name="repositoryLink"
-                                        fullWidth
-                                        autoComplete="repositoryLink"
-                                        disabled={isDisabled}
-                                        onChange={handleRepositoryChanges}
-                                    />
-                                </Grid>
+                        <TextField
+                            required
+                            id="repositoryLink"
+                            name="repositoryLink"
+                            fullWidth
+                            autoComplete="repositoryLink"
+                            disabled={isDisabled}
+                            onChange={handleRepositoryChanges}
+                            value={currentRepository.repositoryLink}
+                        />
+                    </Grid>
                     <Grid item xs={12} sm={2}>
-                                <TextField
-                                    required
-                                    id="productionBranchName"
-                                    name="productionBranchName"
-                                    fullWidth
-                                    autoComplete="productionBranchName"
-                                    disabled={isDisabled}
-                                    onChange={handleRepositoryChanges}
-                                />
-                            </Grid>
+                        <TextField
+                            required
+                            id="productionBranchName"
+                            name="productionBranchName"
+                            fullWidth
+                            autoComplete="productionBranchName"
+                            disabled={isDisabled}
+                            onChange={handleRepositoryChanges}
+                            value={currentRepository.productionBranchName}
+                        />
+                    </Grid>
                     <Grid item xs={12} sm={1} alignItems="center" textAlign={"center"} paddingRight={'22px'}>
                         <Fab color="success" aria-label="add" size="small" onClick={handleAddRepository} >
                             <AddIcon />
@@ -277,7 +311,7 @@ export default function ProjectMonitoring(props) {
                         variant="contained"
                         sx={{ mt: 3, ml: 1 }}
                         type={"submit"}
-                        disabled={isDisabled}
+                        disabled={isDisabled || !enableSaveButton}
                     >Guardar</Button>
                 </Box>
             </Box>
