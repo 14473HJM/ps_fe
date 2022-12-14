777bb63bdb08f7a5d6539e8f90f8a5f82d22313a
14473HJM <14473@tecnicatura.frc.utn.edu.ar>
2022-12-13 23:04:55 -0300

    Refactor projects api

--- pages/api/projects/index.js
+++ pages/api/projects/index.js
@@ -1,27 +1,43 @@
-import { getToken } from 'next-auth/jwt'; 
+import { getToken } from 'next-auth/jwt';
 import axios from 'axios';
 
 const secret = process.env.SECRET;
 
 const handler = async (req, res) => {
-    const { body } = req;
+    const { method, body } = req;
     const token = await getToken({ req, secret });
-    console.log(token);
     const { access_token, userId, roles } = token;
-    body.students = [{ id: userId, objectType: roles[0]}]
     const options = {
         headers: {
             'Authorization': 'Bearer ' + access_token,
         },
     };
 
-    axios.post('http://localhost:8080/ps/projects', body, options)
-        .then(function (response) {
-            res.status(200).json(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    switch (method) {
+        case 'POST':
+            body.students = [{ id: userId, objectType: roles[0] }]
+
+            axios.post('http://localhost:8080/ps/projects', body, options)
+                .then(function (response) {
+                    res.status(200).json(response.data);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            break;
+        case 'PUT':
+            const { id } = body;
+            axios.put(`http://localhost:8080/ps/projects/${id}`, body, options)
+                .then(function (response) {
+                    res.status(200).json(response.data);
+                })
+                .catch(function (error) {
+                    const { status, data } = error.response;
+                    console.log(data);
+                    res.status(status).json(data);
+                });
+            break;
+    }
 };
 
 export default handler;
